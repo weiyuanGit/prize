f0df6c5b7817fe44c97718696812f5dc03b52b63
huqian <2608361955@qq.com>
2019-11-27 16:03:33 +0800

    PrizeVerticle

--- src/main/java/zyxhj/cn/start/PrizeVerticle.java
+++ src/main/java/zyxhj/cn/start/PrizeVerticle.java
@@ -1,6 +1,10 @@
 package zyxhj.cn.start;
 
 import io.vertx.core.Vertx;
+import zyxhj.prize.service.PrizeService;
+import zyxhj.prize.service.ProductService;
+import zyxhj.prize.service.UserService;
+import zyxhj.utils.Singleton;
 import zyxhj.utils.ZeroVerticle;
 
 public class PrizeVerticle extends ZeroVerticle {
@@ -17,6 +21,8 @@ public class PrizeVerticle extends ZeroVerticle {
 		return 8090;
 	}
 	protected void init() throws Exception {
-//		initCtrl(ctrlMap, Singleton.ins(AppraiseService.class, "appraise"));
+		initCtrl(ctrlMap, Singleton.ins(PrizeService.class, "prize"));
+		initCtrl(ctrlMap, Singleton.ins(ProductService.class, "product"));
+		initCtrl(ctrlMap, Singleton.ins(UserService.class, "user"));
 	}
 }
